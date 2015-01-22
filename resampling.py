f5ac840b181b1b6c19aaf78bd7cacde2d7259ae0
rmaptbu <rmaptbu@ucl.ac.uk>
2015-01-22 16:19:49 +0000

    added slow linear interpolation

--- resampling.py
+++ resampling.py
@@ -80,14 +80,55 @@ def resampling_ln(reference,
                   floating,
                   transformation,
                   padding):
-    # TODO
-    return None
+        # Create an empty image based on the reference image discretisation space
+    warped_image=np.zeros(reference.shape)
+    reference_position=np.array([0,0,1])
+    # Iterate over all pixel in the reference image
+    for j in range(0,reference.shape[1]):
+        reference_position[1]=j
+        for i in range(0,reference.shape[0]):
+            reference_position[0]=i
+            # Compute the corresponding position in the floating image space
+            floating_position=transformation.dot(reference_position)
+            # Nearest neighbour interpolation
+            if floating_position[0]>0 and \
+                floating_position[1]>0 and \
+                floating_position[0]<reference.shape[0]-1 and \
+                floating_position[1]<reference.shape[1]-1:
+                #do linear interpolation
+                x_weight=floating_position[0]-np.floor(floating_position[0])
+                y_weight=floating_position[1]-np.floor(floating_position[1])
+                floating_position[0]=np.floor(floating_position[0])
+                floating_position[1]=np.floor(floating_position[1])                    
+                x1_weighted=(1-x_weight)*(1-y_weight)*floating[floating_position[0]][floating_position[1]]
+                x2_weighted=(x_weight)*(1-y_weight)*floating[floating_position[0]+1][floating_position[1]]
+                y1_weighted=(1-x_weight)*(y_weight)*floating[floating_position[0]][floating_position[1]+1]
+                y2_weighted=(x_weight)*(y_weight)*floating[floating_position[0]+1][floating_position[1]+1]
+                warped_image[i][j]=x1_weighted+x2_weighted+y1_weighted+y2_weighted
+            else:
+                warped_image[i][j]=padding
+    return warped_image
 
 # Linear interpolation
 def resampling_ln_fast(reference,
                        floating,
                        transformation):
-    # TODO
+        # Compute the deformation field
+    ref_def = np.ones([3,reference.size])
+    ref_def[0:2,:] = np.mgrid[0:reference.shape[0],0:reference.shape[1]].reshape(2,reference.size)
+    flo_def=transformation.dot(ref_def)
+    
+    # Compute the nearest indices
+    pre = np.round(flo_def).astype('int')
+    indices = pre[0,:] * floating.shape[1] + pre[1,:]
+
+    # Cheap boundary conditions
+    indices[indices<0]=0
+    indices[indices>=floating.size]=floating.size-1
+
+    # Weighted sum
+    warped_image = floating.ravel()[indices]
+    return warped_image.reshape(reference.shape)
     return None
 
 # Sum squared difference
@@ -111,7 +152,7 @@ def main():
     floating_image=read_png_file('./BrainWeb_2D.png')
     
     # Generate and apply a transformation
-    translation_values=np.arange(-2,2,0.05,dtype='float')
+    translation_values=np.arange(-2,2,0.25,dtype='float')
 #    translation_values=np.array([-2])
     transformation_matrix=np.eye(3,3)
     ssd_values_nn=np.ndarray(len(translation_values))
@@ -120,9 +161,9 @@ def main():
     for i in range(0,len(translation_values)):
         print('Perform resampling '+str(i+1)+'/'+str(len(translation_values)))
         transformation_matrix[0][2]=translation_values[i]
-        warped_image_nn=resampling_nn_fast(reference_image,
+        warped_image_nn=resampling_ln(reference_image,
                                                 floating_image,
-                                                transformation_matrix)
+                                                transformation_matrix,0)
         ssd_values_nn[i]=ssd(reference_image,warped_image_nn)
 
     # Display the results
